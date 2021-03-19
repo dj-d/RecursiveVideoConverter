bb11d32305025cddce0a12625c36b84f9fea77c7
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-19 15:27:48 +0100

    Added parameters to specify the type of file to convert and the type to return + Added check for files other than those specified

--- recursive_video_converter.sh
+++ recursive_video_converter.sh
@@ -4,6 +4,9 @@ ORIGINAL_FOLDER="$1"
 
 LOG_FILE_PATH=$ORIGINAL_FOLDER"/status.log"
 
+INPUT_EXT="$2"
+OUTPUT_EXT="$3"
+
 function convert() {
     ffmpeg -i $1 $2 2>/dev/null
 }
@@ -12,22 +15,23 @@ function explore() {
 
     for file in "$1"/* 
         do
-            if ! [[ "$file" == *"Original"* || "$file" == *"Converted"* || "$file" == *"status"* ]] ; then
+            if ! [[ "$file" == *"Original"* || "$file" == *"Converted"* || "$file" == *"NotToConvert"* || "$file" == *"status"* ]] ; then
                 if [ ! -d "${file}" ] ; then
-                    if ! [[ "$file" == *"Original"* || "$file" == *"Converted"* ]] ; then
+
+                    file_name=$(echo "${file#"$1"}")
+                    file_name=${file_name:1}
+                    
+                    if [[ "$file" == *"$INPUT_EXT"* ]] ; then
                         mkdir -p Original
                         mkdir -p Converted
 
-                        file_name=$(echo "${file#"$1"}")
-                        file_name=${file_name:1}
-
-                        renamed_file=${file_name:0:-3}"mp4"
+                        renamed_file=${file_name:0:-3}$OUTPUT_EXT
 
                         if [ ! -f "${renamed_file}" ] ; then
                             echo "############### File: ${file_name}" >> $LOG_FILE_PATH
                             echo "#################### Start conversion" >> $LOG_FILE_PATH
 
-                            convert $file_name $renamed_file
+                            # convert $file_name $renamed_file
                             
                             echo "#################### Finisch conversion" >> $LOG_FILE_PATH
                         fi;
@@ -39,10 +43,14 @@ function explore() {
                         if [ -f "$renamed_file" ] ; then
                             mv $renamed_file Converted/.
                         fi;
+                    else
+                        mkdir -p NotToConvert
+
+                        mv $file_name NotToConvert/.
                     fi;
                 else
                     
-                    echo "########## Folder: ${file} -----" >> $LOG_FILE_PATH
+                    echo "########## Folder: ${file}" >> $LOG_FILE_PATH
 
                     cd "${file}"
 
