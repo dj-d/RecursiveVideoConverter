2bdb3f93efa0e56aa8ee64e479890456f89d115f
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-20 23:16:46 +0100

    Added 'USE_GPU' option + added option 'LOG_FILE_PATH' + Code Refactoring + Reformatting

--- recursive_video_converter.sh
+++ recursive_video_converter.sh
@@ -1,75 +1,86 @@
 #! /bin/bash
 
-INIT_FOLDER=""
-
-LOG_FILE_PATH=$INIT_FOLDER"/status.log"
-
+PATH=""
 INPUT_EXT=""
 OUTPUT_EXT=""
+USE_GPU=false
+
+LOG_FILE_PATH="${pwd}status.log"
 
 ORIGINAL_FOLDER="Original"
 CONVERTED_FOLDER="Converted"
 NOTTOCONVERT_FOLDER="NotToConvert"
 STATUS_FILE="status"
 
-USE_GPU=false
-
 function convert() {
-    ffmpeg -i $1 $2 2>/dev/null
+    if [ $1 = true ] 
+        then
+            ffmpeg -hwaccel cuda -i $2 $3 2>/dev/null
+        else
+            ffmpeg -i $2 $3 2>/dev/null
+        fi;
 }
 
 function explore() {
     for file in "$1"/* 
         do
             # Folders and files to avoid
-            if ! [[ "$file" == *$ORIGINAL_FOLDER* || "$file" == *$CONVERTED_FOLDER* || "$file" == *$NOTTOCONVERT_FOLDER* || "$file" == *$STATUS_FILE* ]] ; then
+            if ! [[ "$file" == *$ORIGINAL_FOLDER* || "$file" == *$CONVERTED_FOLDER* || "$file" == *$NOTTOCONVERT_FOLDER* || "$file" == *$STATUS_FILE* ]] 
+                then
+                    # Check if I am analyzing a folder or a file
+                    if [ ! -d "${file}" ] 
+                        then
+                            file_name=$(echo "${file#"$1"}")
+                            file_name=${file_name:1}
+                            
+                            # To take into account only files with the extension specified in the input
+                            if [[ "$file" == *"$2"* ]] 
+                                then
+                                    mkdir -p $ORIGINAL_FOLDER
+                                    mkdir -p $CONVERTED_FOLDER
 
-                # Check if I am analyzing a folder or a file
-                if [ ! -d "${file}" ] ; then
-                    file_name=$(echo "${file#"$1"}")
-                    file_name=${file_name:1}
-                    
-                    # To take into account only files with the extension specified in the input
-                    if [[ "$file" == *"$INPUT_EXT"* ]] ; then
-                        mkdir -p $ORIGINAL_FOLDER
-                        mkdir -p $CONVERTED_FOLDER
+                                    renamed_file=${file_name:0:-3}$3
 
-                        renamed_file=${file_name:0:-3}$OUTPUT_EXT
+                                    # If the converted version of the file to be converted does not already exist
+                                    if [ ! -f "${renamed_file}" ] 
+                                        then
+                                            echo "############### File: ${file_name}" >> "${5}"
+                                            echo "#################### Start conversion" >> "${5}"
 
-                        # If the converted version of the file to be converted does not already exist
-                        if [ ! -f "${renamed_file}" ] ; then
-                            echo "############### File: ${file_name}" >> $LOG_FILE_PATH
-                            echo "#################### Start conversion" >> $LOG_FILE_PATH
+                                            convert $4 $2 $3
 
-                            convert $file_name $renamed_file
+                                            echo "#################### Finisch conversion" >> "${5}"
+                                        fi;
 
-                            echo "#################### Finisch conversion" >> $LOG_FILE_PATH
+                                    if [ -f "$file_name" ] 
+                                        then
+                                            mv $file_name $ORIGINAL_FOLDER"/."
+                                        fi;
+
+                                    if [ -f "$renamed_file" ] 
+                                        then
+                                            mv $renamed_file $CONVERTED_FOLDER"/."
+                                        fi;
+                                else
+                                    mkdir -p $NOTTOCONVERT_FOLDER
+
+                                    mv $file_name $NOTTOCONVERT_FOLDER"/."
+                                fi;
+                        else
+                            echo "########## Folder: ${file}" >> "${5}"
+
+                            cd "${file}"
+
+                            explore "${file}"
                         fi;
-
-                        if [ -f "$file_name" ] ; then
-                            mv $file_name $ORIGINAL_FOLDER"/."
-                        fi;
-
-                        if [ -f "$renamed_file" ] ; then
-                            mv $renamed_file $CONVERTED_FOLDER"/."
-                        fi;
-                    else
-                        mkdir -p $NOTTOCONVERT_FOLDER
-
-                        mv $file_name $NOTTOCONVERT_FOLDER"/."
-                    fi;
-                else
-                    echo "########## Folder: ${file}" >> $LOG_FILE_PATH
-
-                    cd "${file}"
-
-                    explore "${file}"
                 fi;
-            fi;
         done
 }
 
-function usage() { echo "Usage: $0 " 1>&2; exit 1; }
+function usage() { 
+    echo "Usage: $0 " 1>&2
+    exit 1
+}
 
 function help() {
     echo "HELP"
@@ -77,24 +88,37 @@ function help() {
 }
 
 function main() {
-    echo "##### START" >> $LOG_FILE_PATH
-
-    # explore "${1}"
+    if [[ -z "${1}" && -z "${2}" && -z "${3}" ]]
+        then
+            echo "Empty param"
+            exit 1
+        fi;
     
-    echo "##### FINISH" >> $LOG_FILE_PATH
+    echo "${5}"
+
+    echo "##### START" >> "${5}"
+
+    explore "${1}" "${2}" "${3}" $4 "${5}"
+
+    echo "##### FINISH" >> "${5}"
+
+    exit 0
 }
 
-while getopts "p:i:o:gh" option ; 
+while getopts "p:i:o:l:gh" option ; 
     do
         case "${option}" in
             p)
-                INIT_FOLDER="${OPTARG}"
+                PATH="${OPTARG}"
                 ;;
             i)
                 INPUT_EXT="${OPTARG}"
                 ;;
             o)
                 OUTPUT_EXT="${OPTARG}"
+                ;;
+            l)
+                LOG_FILE_PATH="${OPTARG}status.log"
                 ;;
             g)
                 USE_GPU=true
@@ -110,4 +134,4 @@ while getopts "p:i:o:gh" option ;
 
 shift $((OPTIND-1))
 
-main $INIT_FOLDER
+main $PATH $INPUT_EXT $OUTPUT_EXT $USE_GPU $LOG_FILE_PATH &
